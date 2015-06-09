e5f36c62b4954a06e05eeff058790a248ed07e08
Justin Bakse <jbakse@brandnewschool.com>
2015-06-09 15:31:45 -0400

    added serial port

--- src/speed_rail.js
+++ src/speed_rail.js
@@ -8,9 +8,37 @@ app.use(express.static('public'));
 
 var server = app.listen(3000, function () {
 
-  var host = server.address().address;
-  var port = server.address().port;
+	var host = server.address().address;
+	var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
+	console.log('Example app listening at http://%s:%s', host, port);
 
 });
+
+
+
+
+var SerialPort = require("serialport").SerialPort;
+
+var serialPort = new SerialPort("/dev/cu.usbserial-DA00XX95", {
+	baudrate: 115200
+});
+
+
+serialPort.on("open", function () {
+	console.log('open');
+	
+	serialPort.on('data', function(data) {
+		console.log('data received: ' + data);
+	});
+	
+	serialPort.write("g0 x10 \n", function(err, results) {
+		console.log('err ' + err);
+		console.log('results ' + results);
+	});
+});
+
+
+
+
+
